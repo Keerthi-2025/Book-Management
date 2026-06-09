931d182122c77db4e818dcefdb4b28eb63e26582
Keerthi <rhkeerthi3@gmail.com>
2026-06-09 22:01:54 +0530

    completed user repository

--- Book/src/main/java/com/repositories/UserRepository.java
+++ Book/src/main/java/com/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.repositories;
 
-public interface UserRepository {
+import com.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
