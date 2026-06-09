6c6fb68b5b1ae3d1e854db7e12ac79fa2ccd5a52
Keerthi <rhkeerthi3@gmail.com>
2026-06-09 22:49:18 +0530

    added exceptions

---
+++ Book/src/main/java/com/exceptions/ApiRequestExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiRequestExceptionHandler {
+
+    @ExceptionHandler(ApiRequestException.class)
+    public ResponseEntity<ApiRequestExceptionModel> handleApiRequestException(
+            ApiRequestException ex) {
+
+        ApiRequestExceptionModel error = new ApiRequestExceptionModel(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
