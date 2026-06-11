7275b2177464f87795074960a5779df70f455b8c
Keerthi <rhkeerthi3@gmail.com>
2026-06-11 15:27:38 +0530

    modified files

--- Book/src/main/java/com/book/Book/entities/Book.java
+++ Book/src/main/java/com/book/Book/entities/Book.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID book_Id;
 
     @Column
--- Book/src/main/java/com/book/Book/mappers/BookMapper.java
+++ Book/src/main/java/com/book/Book/mappers/BookMapper.java
@@ -1,9 +1,21 @@
 package com.book.Book.mappers;
 
 
+import com.book.Book.BookStatus;
+import com.book.Book.Dto.Request.CreateBookDto;
+import com.book.Book.entities.Book;
 import org.springframework.stereotype.Component;
 
 @Component
 
 public class BookMapper {
+    public Book toBook(CreateBookDto dto){
+        return Book.builder()
+                .title(dto.title())
+                .author(dto.author())
+                .genre(dto.genre())
+                .imageUrl(dto.imageUrl())
+                .status(BookStatus.AVAILABLE)
+                .build();
+    }
 }
