aa98d4dd2178e7038d6c60645a0501d3461cf32e
Keerthi <rhkeerthi3@gmail.com>
2026-06-11 15:23:11 +0530

    created bookmapper

---
+++ Book/src/main/java/com/book/Book/mappers/BookMapper.java
@@ -0,0 +1,9 @@
+package com.book.Book.mappers;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class BookMapper {
+}
---
+++ Book/src/main/java/com/book/Book/services/book_service/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.book.Book.services.book_service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class BookServiceImpl {
+
+}
