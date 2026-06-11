134545b945d68d61cbdca6a4b58af54c94569c4d
Keerthi <rhkeerthi3@gmail.com>
2026-06-11 15:17:02 +0530

    completed bookservice

---
+++ Book/src/main/java/com/book/Book/services/book_service/BookService.java
@@ -0,0 +1,22 @@
+package com.book.Book.services.book_service;
+
+import com.book.Book.Dto.Request.CreateBookDto;
+import com.book.Book.entities.Book;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookService {
+
+     String addBook(CreateBookDto dto);
+
+     String updateBook(UUID book_Id, CreateBookDto dto);
+
+     String deleteBook(UUID book_Id);
+
+     List<Book> getAllBooks();
+
+     Book getBookByTitle(String title);
+
+     List<Book>searchBooks(String title);
+}
