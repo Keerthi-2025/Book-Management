bff47632410aae162f8ba3fb9ec41dc2a4de91cf
Keerthi <rhkeerthi3@gmail.com>
2026-06-13 23:56:56 +0530

    added adminsignup img

--- Frontend/book/app/Auth/admin-signup/page.jsx
+++ Frontend/book/app/Auth/admin-signup/page.jsx
@@ -37,7 +37,12 @@ export default function AdminSignup() {
     }
 
     return(
-    <div >
+        <div
+    className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/images/login.jpeg')",
+      }}
+      >
         <div className="w-[400px] p-8 rounded-xl shadow-lg bg-blue-400 mt-40 ml-100">
             <h1 className="text-3xl font-bold text-white flex justify-center">Admin Signup</h1>
 
