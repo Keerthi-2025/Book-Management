5210ba23ee7f0fa174e201a86ad52225380fee9d
Keerthi <rhkeerthi3@gmail.com>
2026-06-14 23:52:03 +0530

    modified userApi

--- Frontend/book/app/lib/api/userApi.js
+++ Frontend/book/app/lib/api/userApi.js
@@ -2,7 +2,7 @@ import axiosInstance from "@/app/utils/axiosInstance";
 
 const getAllUsers = async () =>{
     try {
-        const response = await axiosInstance.get("/User/v1/getAllUsers",  data);
+        const response = await axiosInstance.get("/User/v1/getAllUsers");
         return response.data;
 
         
@@ -14,7 +14,11 @@ const getAllUsers = async () =>{
 
 const getUserById = async () =>{
     try {
-        const response = await axiosInstance.get("/User/v1/getUserById", data);
+        const response = await axiosInstance.get("/User/v1/getUserById", {
+            params:{
+                user_Id: user_Id,
+            },
+        });
         return response.data;
         
     } catch (error) {
