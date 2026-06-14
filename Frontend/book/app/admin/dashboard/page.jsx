9e156ce8127d92c23d7fd9b3e132a20744869c9c
Keerthi <rhkeerthi3@gmail.com>
2026-06-14 21:48:03 +0530

    modifed admindashboard design

--- Frontend/book/app/admin/dashboard/page.jsx
+++ Frontend/book/app/admin/dashboard/page.jsx
@@ -7,17 +7,17 @@ export default function AdminDashboard () {
         <div className="bg-gray-500 min-h-screen">
 
         
-        <div className="">
+        <div className="flex-1 p-10">
         <h1 className="text-black font-bold flex justify-center">Admin Dashboard</h1>
     </div>
 
-        <div className="grid grid-cols-2 gap-2">
-            <div className="text-white font-bold border p-4 mt-2 bg-black flex justify-center rounded-2xl">
+        <div className="grid grid-cols-2 gap-20">
+            <div className="text-white font-bold border p-4 mt-2 w-100 bg-black flex justify-center rounded-2xl">
                 <h2>Total Users</h2>
             </div>
 
 
-            <div className="text-white bg-black border flex justify-center font-bold p-4 mt-2 rounded-2xl">
+            <div className="text-white bg-black border flex justify-center w-100 font-bold p-4 mt-2 rounded-2xl">
                 <h2>Total Books</h2>
             </div>
         </div>
