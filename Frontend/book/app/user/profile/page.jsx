e1eccd7d009d43a9f845b78435d9eecf670a00fd
Keerthi <rhkeerthi3@gmail.com>
2026-06-20 23:04:40 +0530

    created profile file

---
+++ Frontend/book/app/user/profile/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ProfilePage() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+
+  }, []);
+
+  return (
+    <div className="p-8">
+
+      <h1 className="text-3xl font-bold mb-6">
+        My Profile
+      </h1>
+
+      {user && (
+        <div className="bg-white p-6 rounded-xl shadow w-96">
+
+          <p>
+            <strong>Name:</strong> {user.userName}
+          </p>
+
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+
+          <p>
+            <strong>Role:</strong> {user.role}
+          </p>
+
+        </div>
+      )}
+
+    </div>
+  );
+}
